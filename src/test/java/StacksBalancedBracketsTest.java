8e099af768c52f261629ce4997c716dd9dc69c5a
TSAKIRIS Fotis <fotis.tsakiris@intrasoft-intl.com>
2017-02-19 19:04:47 +0200

    Stacks Balanced Brackets v1 vector

---
+++ src/test/java/StacksBalancedBracketsTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StacksBalancedBracketsTest {
+    @Test
+    public void isBalanced() throws Exception {
+        Assert.assertEquals(true, StacksBalancedBrackets.isBalanced("{[()]}"));
+        Assert.assertEquals(false, StacksBalancedBrackets.isBalanced("{[(])}"));
+        Assert.assertEquals(true, StacksBalancedBrackets.isBalanced("{{[[(())]]}}"));
+    }
+}
