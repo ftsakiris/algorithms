faa7b7cc7cbf8274d7e380eaab8668584807c52f
TSAKIRIS Fotis <fotis.tsakiris@intrasoft-intl.com>
2017-02-27 19:47:35 +0200

    Fibonacci

---
+++ src/test/java/FibonacciTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class FibonacciTest {
+    @Test
+    public void fibonacci() throws Exception {
+        System.out.println(Fibonacci.fibonacci(0));
+        System.out.println(Fibonacci.fibonacci(1));
+        System.out.println(Fibonacci.fibonacci(2));
+        System.out.println(Fibonacci.fibonacci(3));
+        System.out.println(Fibonacci.fibonacci(4));
+        System.out.println(Fibonacci.fibonacci(5));
+        System.out.println(Fibonacci.fibonacci(6));
+    }
+
+}
